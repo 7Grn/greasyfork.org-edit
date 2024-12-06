5dbc9aa181f6c97060f11ab2d2dd16d212a0c25b
7Grn <154948822+7Grn@users.noreply.github.com>
2024-12-06 17:19:00 +0900

    Update scripts_371641.user.js

--- scripts_371641.user.js
+++ scripts_371641.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Video Background Play Fix_371641
 // @namespace    https://greasyfork.org/en/users/50-couchy
-// @version      20220119.edit.7
+// @version      20220119.edit.9
 // @description  Prevents YouTube and Vimeo from pausing videos when minimizing or switching tabs. Cross-browser port of https://addons.mozilla.org/en-US/firefox/addon/video-background-play-fix/
 // @author       Couchy
 // @match        *://*.youtube.com/*
